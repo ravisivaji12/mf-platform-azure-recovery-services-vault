ff1b6a5dd4119882d26011fae94b832c08900261
Ravi, Sivaji <ravi.sivaji@outlook.com>
2025-04-21 22:28:24 -0400

    Changes

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+
+# output "resource" {
+#   description = "resource Id output"
+#   value       = azurerm_recovery_services_vault.resource
+# }
+
+# # Module owners should include the full resource via a 'resource' output
+# # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
+# output "resource_id" {
+#   description = "resource Id output"
+#   value       = azurerm_recovery_services_vault.resource_id
+# }
---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+  required_providers {
+    # TODO: Ensure all required providers are listed here.
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.107.0"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0"
+    }
+  }
+}
